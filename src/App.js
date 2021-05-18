3344ac2c3b1a5c8d7eec8e10a46fa9169bd38a94
Xiaolu Deng <sxiaolu86@gmail.com>
2021-05-18 17:37:58 -0600

    19. Clear users form state

--- src/App.js
+++ src/App.js
@@ -23,6 +23,9 @@ class App extends Component {
   //   this.setState({ users: res.data, loading: false });
   // }
 
+  // Clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
+
   // Search Github users
   searchUsers = async text => {
     this.setState({ loading: true });
@@ -36,12 +39,18 @@ class App extends Component {
   }
 
   render() {
+    const { users, loading } = this.state;
+
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Search searchUsers={this.searchUsers} />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search 
+            searchUsers={this.searchUsers} 
+            clearUsers={this.clearUsers} 
+            showClear={users.length > 0 ? true: false }
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
