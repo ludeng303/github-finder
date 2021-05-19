2f6395b620288f5c382450ad18161ac99a898061
Xiaolu Deng <sxiaolu86@gmail.com>
2021-05-19 09:50:42 -0600

    22. Single User Component % Data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment, Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
@@ -11,21 +12,10 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   };
-  
-  // async componentDidMount() {
-  //   this.setState({ loading: true });
-
-  //   const res = await axios.get(
-  //     `https://api.github.com/users?client_id=${
-  //       process.env.REACT_APP_GITHUB_CLIENT_ID
-  //     }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-  //   );
-
-  //   this.setState({ users: res.data, loading: false });
-  // }
 
   // Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
@@ -49,8 +39,21 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   }
 
+    // Get single Github user
+    getUser = async (username) => {
+      this.setState({ loading: true });
+
+      const res = await axios.get(
+        `https://api.github.com/users/${username}?&client_id=${
+          process.env.REACT_APP_GITHUB_CLIENT_ID
+        }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      );
+  
+      this.setState({ user: res.data, loading: false });
+    }
+
   render() {
-    const { users, loading } = this.state;
+    const { users, user, loading } = this.state;
 
     return (
       <Router>
@@ -75,6 +78,9 @@ class App extends Component {
                 )} 
               />
               <Route exact path='/about' component={About} />
+              <Route exact path='/user/:login' render={props => (
+                <User { ...props } getUser={this.getUser} user={user} loading={loading} />
+              )} />
             </Switch>
           </div>
         </div>
@@ -84,3 +90,15 @@ class App extends Component {
 }
 
 export default App;
+
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${
+  //       process.env.REACT_APP_GITHUB_CLIENT_ID
+  //     }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
+
+  //   this.setState({ users: res.data, loading: false });
+  // }
