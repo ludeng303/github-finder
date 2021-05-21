482098d3c4985d531e39dc9ca63086229acd3a3b
Xiaolu Deng <sxiaolu86@gmail.com>
2021-05-21 07:54:11 -0600

    30. Create Reducer & Action

--- src/App.js
+++ src/App.js
@@ -19,21 +19,7 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
 
-  // Search Github users
-  const searchUsers = async text => {
-    setLoading(true);
-
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${
-        process.env.REACT_APP_GITHUB_CLIENT_ID
-      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    setUsers(res.data.items);
-    setLoading(false);
-  };
-
-  // Clear users from state
+    // Clear users from state
   const clearUsers = () => {
     setUsers([]);
     setLoading(false);
@@ -87,7 +73,6 @@ const App = () => {
                 render={props => (
                   <Fragment>
                     <Search 
-                      searchUsers={searchUsers} 
                       clearUsers={clearUsers} 
                       showClear={users.length > 0 ? true: false }
                       setAlert={showAlert}
